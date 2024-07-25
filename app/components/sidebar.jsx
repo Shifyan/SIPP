ca147bc9ff3dada6cecd0a4f8aa1dc4e22171d92
Shifyannn <asifyan01@gmail.com>
2024-07-25 17:45:16 +0700

    perbaikan komponen sidebar

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -3,11 +3,13 @@ export default function SideBar({ isOpen }) {
   return (
     <>
       {isOpen ? (
-        <div className="block bg-white shadow-md w-64 border-e-2 border-black">
+        <div className="block p-5 bg-white shadow-md w-64 border-e-2 border-black">
           {/* Sidebar content */}
-          <div className="px-4 py-6">
-            <h2 className="text-lg font-semibold">Sidebar</h2>
-            <ul className="mt-4">
+          <div className="px-4 py-6 flex flex-col justify-center">
+            <a href="#">
+              <h2 className=" font-bold text-2xl">Sidebar</h2>
+            </a>
+            <ul className="mt-4 text-2xl font-bold">
               <li className="py-2">
                 <a
                   href="#"
@@ -21,7 +23,8 @@ export default function SideBar({ isOpen }) {
                   href="#"
                   className="flex items-center text-gray-700 hover:text-gray-900"
                 >
-                  <HiChartSquareBar className="mr-2" /> Dashboard
+                  <HiChartSquareBar className="mr-2" />
+                  Keaktifan
                 </a>
               </li>
               {/* Add more sidebar items here */}
@@ -29,7 +32,32 @@ export default function SideBar({ isOpen }) {
           </div>
         </div>
       ) : (
-        <div className="block bg-white shadow-md w-20 border-e-2 border-black"></div>
+        <div className="block bg-white shadow-md w-20 border-e-2 border-black">
+          <div className="px-4 py-6 flex flex-col justify-center">
+            <a href="#">
+              <h1 className="text-lg font-semibold">SIPP</h1>
+            </a>
+            <ul className="mt-4 text-2xl font-bold">
+              <li className="py-2">
+                <a
+                  href="#"
+                  className="flex items-center text-gray-700 hover:text-gray-900"
+                >
+                  <HiHome className="mr-2" />
+                </a>
+              </li>
+              <li className="py-2">
+                <a
+                  href="#"
+                  className="flex items-center text-gray-700 hover:text-gray-900"
+                >
+                  <HiChartSquareBar className="mr-2" />
+                </a>
+              </li>
+              {/* Add more sidebar items here */}
+            </ul>
+          </div>
+        </div>
       )}
     </>
   );
