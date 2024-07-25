3567c93e0d77beb71980b95a67b413c41cb27f95
Shifyannn <asifyan01@gmail.com>
2024-07-26 00:56:35 +0700

    mencoba mengatasi masalah sidebar

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -4,31 +4,14 @@ export default function SideBar({ isOpen }) {
     <>
       {isOpen ? (
         <div className="block p-5 bg-white shadow-md w-64 border-e-2 border-black">
-          {/* Sidebar content */}
-          <div className="px-4 py-6 flex flex-col justify-center">
-            <a href="#">
-              <h2 className=" font-bold text-2xl">Sidebar</h2>
+          <div className="p-9 border-2 border-black flex justify-center flex-col">
+            <a href="/user/home">
+              <h1>SIPP</h1>
             </a>
-            <ul className="mt-4 text-2xl font-bold">
-              <li className="py-2">
-                <a
-                  href="#"
-                  className="flex items-center text-gray-700 hover:text-gray-900"
-                >
-                  <HiHome className="mr-2" /> Home
-                </a>
-              </li>
-              <li className="py-2">
-                <a
-                  href="#"
-                  className="flex items-center text-gray-700 hover:text-gray-900"
-                >
-                  <HiChartSquareBar className="mr-2" />
-                  Keaktifan
-                </a>
-              </li>
-              {/* Add more sidebar items here */}
-            </ul>
+            <div className="flex">
+              <HiHome></HiHome>
+              <HiChartSquareBar></HiChartSquareBar>
+            </div>
           </div>
         </div>
       ) : (
